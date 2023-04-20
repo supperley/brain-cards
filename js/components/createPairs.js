3a869de95e0f5dd10d152389a7517ae5453a50e1
supperley <v.bobrovko@gmail.com>
2023-04-20 17:05:57 +0300

    control createPairs

--- js/components/createPairs.js
+++ js/components/createPairs.js
@@ -1,4 +1,5 @@
 import { createElement } from '../helper/createElement.js';
+import { showAlert } from './showAlert.js';
 
 export const createPairs = (app) => {
     const pairs = createElement('section', {
@@ -32,18 +33,43 @@ export const createPairs = (app) => {
     container.append(buttonReturn, buttonCard);
     pairs.append(container);
 
-    const createCard = (data) => {
-        buttonCard.addEventListener('click', () => {
-            console.log('qwerty');
-            front.classList.toggle('card__front');
-            back.classList.toggle('card__back');
-        });
+    const cardController = (data) => {
+        let index = 0;
+
+        front.textContent = data[index][0];
+        back.textContent = data[index][1];
+
+        const flipCard = () => {
+            buttonCard.classList.add('card__item_flipped');
+            buttonCard.removeEventListener('click', flipCard);
+            setTimeout(() => {
+                buttonCard.classList.remove('card__item_flipped');
+                setTimeout(() => {
+                    index++;
+                    if (index == data.length) {
+                        front.textContent = 'the end';
+                        showAlert('Вернемся к категориям!', 2000);
+                        setTimeout(() => {
+                            buttonReturn.click();
+                        }, 2000);
+                        return;
+                    }
+                    front.textContent = data[index][0];
+                    back.textContent = data[index][1];
+                    setTimeout(() => {
+                        buttonCard.addEventListener('click', flipCard);
+                    }, 200);
+                }, 100);
+            }, 2000);
+        };
+
+        buttonCard.addEventListener('click', flipCard);
     };
 
     const mount = (data) => {
         app.append(pairs);
         console.log(data);
-        createCard(data.pairs[0]);
+        cardController(data.pairs);
     };
 
     const unmount = () => {
